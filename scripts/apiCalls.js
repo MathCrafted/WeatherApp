554653ab09e2687a205e722d87cd6c4a10f1aaa7
MathCrafted <109101387+MathCrafted@users.noreply.github.com>
2025-02-24 22:39:11 -0600

    adds dynamic icons to indicate weather

--- scripts/apiCalls.js
+++ scripts/apiCalls.js
@@ -2,20 +2,58 @@
 
 // Define function to call weather api
 async function getWeatherData() {
-    const urlAPICall = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,precipitation,cloud_cover&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=1"
-    // fetch
-    const objApiResponse = await (await fetch(urlAPICall)).json()
-    console.log(objApiResponse)
+    const urlAPICall = "https://api.open-meteo.com/v1/forecast?latitude=36.17&longitude=-85.5&current=temperature_2m,relative_humidity_2m,precipitation,weather_code&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch"
+    try {
+        // fetch
+        const objApiResponse = await fetch(urlAPICall)
 
-    // update background
-    if(objApiResponse.current.cloud_cover) {
+        // verify ok
+        if(!objApiResponse.ok) {
+            throw new Error('HTTP Error Status:', objApiResponse)
+        }
 
+        // get json
+        const objApiJson = await objApiResponse.json()
+        console.log(objApiJson)
+
+
+        if(objApiJson.current.weather_code >= 2) {
+            // update background
+            document.body.style = "background-image: linear-gradient(to bottom, lightgray, darkgray)"
+            
+        }
+
+        // update temp
+        document.getElementById('pTemperature').innerHTML = objApiJson.current.temperature_2m + ' ' + objApiJson.current_units.temperature_2m
+        if(objApiJson.current.temperature_2m >= 80) {
+            document.getElementById('imgTemperature').src = 'img/thermometer-add.svg'
+        } else if(objApiJson.current.temperature_2m <= 50) {
+            document.getElementById('imgTemperature').src = 'img/thermometer-minus.svg'
+        }
+
+        // update humidity
+        document.getElementById('pHumidity').innerHTML = objApiJson.current.relative_humidity_2m + objApiJson.current_units.relative_humidity_2m + ' Humiditiy'
+        if(objApiJson.current.relative_humidity_2m >= 50) {
+            document.getElementById('imgHumidity').src = 'img/foggy.svg'
+        }
+        
+        // update precipitation
+        document.getElementById('pPrecipitation').innerHTML = objApiJson.current.precipitation + ' ' + objApiJson.current_units.precipitation
+        if (objApiJson.current.weather_code >= 2) {
+            document.getElementById('imgPrecipitation').src = 'img/partly-cloudy.svg'
+        }
+        if (objApiJson.current.weather_code >= 51) {
+            document.getElementById('imgPrecipitation').src = 'img/rainy.svg'
+        }
+
+    } catch(objError) {
+        console.log("Error fetching weather data: ", objError)
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops! Something went wrong!',
+            html: '<p>Please try again later</p>'
+        })
     }
-
-    // update temp
-
-    // update humidity
-    // update precipitation
 }
 
 // Make the API Call
